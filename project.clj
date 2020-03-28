16d30f2e175151d6e9e7068deca93bd87bec8af5
Circle CI <circleci@infrablocks.io>
2020-03-28 13:11:23 +0000

    Pre-release version 0.1.0-RC1 [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/liberator-hal.discovery-resource "0.1.0-RC0"
+(defproject io.logicblocks/liberator-hal.discovery-resource "0.1.0-RC1"
   :description "A HAL discovery resource for liberator"
   :url "https://github.com/logicblocks/liberator-hal.discovery-resource"
 
