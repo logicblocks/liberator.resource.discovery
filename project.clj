67b7b55daf2d883169b4d0ff89a7340f466ffae5
Circle CI <circleci@logicblocks.io>
2023-12-12 01:22:36 +0000

    Pre-release version 0.2.0-RC1 [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/liberator.resource.discovery "0.2.0-RC0"
+(defproject io.logicblocks/liberator.resource.discovery "0.2.0-RC1"
   :description "A discovery resource for liberator."
   :url "https://github.com/logicblocks/liberator.resource.discovery"
 
